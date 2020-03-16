ff12d7d111fd139d1436ed9a221759290e1fa8be
AhmedELGAMMUDI <ahmedbaset092@gmail.com>
2020-03-16 21:29:34 +0300

    server c++

---
+++ site.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    int n=0;
+    while(0<1){
+        while(n<10){
+            system("start https://www.ahmedelgammudi.cf/");
+            Sleep(1000);
+            n++;
+        }
+        Sleep(2000);
+        system("taskkill /IM microsoftEdge.exe /F");
+        n=0;
+        Sleep(2000);}
+
+
+    return 0;
+}
